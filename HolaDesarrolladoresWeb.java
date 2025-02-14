6def8260205589415d96d20e7e040474eec33f13
jxna2112 <jxna2112@users.noreply.github.com>
2025-02-14 12:18:08 +0100

    Actualizar mensaje en HolaDesarrolladoresWeb.java

--- HolaDesarrolladoresWeb.java
+++ HolaDesarrolladoresWeb.java
@@ -1,2 +1,1 @@
-public class HolaDesarrolladoresWeb {\n public static void main(String[] args) {\n
-System.out.println("Hola, desarrolladores de GitHub!");\n }\n}
+public class HolaDesarrolladoresWeb {\n public static void main(String[] args) {\n System.out.println("Hola, nueva funcionalidad en desarrollo!");\n }\n}
